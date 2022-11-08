4ed847a211148779476568fa7f68185f06dbccf5
HM <hieumdd@gmail.com>
2022-11-08 19:33:13 +0700

    chore: Remove other codes

--- src/storage/storage.service.test.ts
+++
@@ -1,9 +0,0 @@
-import { createExportCSV } from './storage.service';
-
-it('Download Link', () => {
-    const file = createExportCSV();
-    const url = file.publicUrl();
-
-    console.log(url)
-    return expect(url).toBeTruthy();
-});
