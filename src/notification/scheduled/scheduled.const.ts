ab47e03350088fab3940167865e0dce44307d8c3
HM <hieumdd@gmail.com>
2022-11-11 15:58:10 +0700

    chore: Remove alert/scheduled module

--- src/notification/scheduled/scheduled.const.ts
+++
@@ -1,5 +0,0 @@
-export const scheduledDateRanges = [
-    { startDate: 'today', endDate: 'today' },
-    { startDate: '7daysAgo', endDate: '7daysAgo' },
-    { startDate: '28daysAgo', endDate: '28daysAgo' },
-];
--- src/notification/scheduled/scheduled.service.ts
+++
@@ -1,9 +0,0 @@
-import * as SingleMetric from './report/single-metric.report';
-import * as TopTag from './report/top-tag.report';
-
-export const scheduleds = [
-    SingleMetric.generic,
-    SingleMetric.reddit,
-    TopTag.generic,
-    TopTag.reddit,
-];
